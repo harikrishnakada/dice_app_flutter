be1d4d3d619c14c64cd957d5614bd76bafb31f55
Hari Krishna <harikrishna.kada@gmail.com>
2022-10-12 23:15:21 +0530

    Updated code to populate dice numbers

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +17,50 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: MaterialButton(
+              onPressed: () {
+                setState(() {
+                  UpdateDiceNumbers();
+                });
+              },
+              child: Image.asset('assets/images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: MaterialButton(
+              onPressed: () {
+                setState(() {
+                  UpdateDiceNumbers();
+                });
+              },
+              child: Image.asset('assets/images/dice$rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void UpdateDiceNumbers() {
+    leftDiceNumber = Random().nextInt(6) + 1;
+    rightDiceNumber = Random().nextInt(6) + 1;
+    print(
+        'Left Dice Number: Right Dice Number => $leftDiceNumber : $rightDiceNumber');
   }
 }
